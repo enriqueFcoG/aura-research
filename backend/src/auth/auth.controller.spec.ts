3c1cc4d7b74ddac374d7734bd18c38cd798f9769
enriqueFcoG <enrikefrancisco@gmail.com>
2025-12-06 20:58:37 -0800

    backend/fix: initial unit tests fixed (dependency injection error)

--- backend/src/auth/auth.controller.spec.ts
+++ backend/src/auth/auth.controller.spec.ts
@@ -1,6 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { UsersService } from '../users/users.service';
 
 describe('AuthController', () => {
   let controller: AuthController;
@@ -8,7 +9,7 @@ describe('AuthController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
-      providers: [AuthService],
+      providers: [AuthService, UsersService],
     }).compile();
 
     controller = module.get<AuthController>(AuthController);
