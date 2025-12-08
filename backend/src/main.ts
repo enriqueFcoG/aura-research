316dd91ae9503204a0138eefc73623af35291952
enriqueFcoG <enrikefrancisco@gmail.com>
2025-12-08 15:13:48 -0800

    backend/feat: cors enabled

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,7 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:3000',//frontend URL, TODO: change in production
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
   app.setGlobalPrefix('api');
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
