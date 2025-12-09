90eb8748020c626ae37cbc976b623f9f77f282d4
enriqueFcoG <enrikefrancisco@gmail.com>
2025-12-09 13:39:41 -0800

    frontend/enhancement: api calls moved from auth actions

--- frontend/src/actions/auth/login.ts
+++ frontend/src/actions/auth/login.ts
@@ -1,5 +1,7 @@
 "use server"
 import { LoginState, SigninFormSchema } from "@/app/lib/definitions";
+import { createSession } from "@/lib/server/session";
+import { authenticateUser } from "@/services/auth.service";
 import { redirect } from "next/navigation";
 
 export async function signin(prevState: LoginState , formData: FormData): Promise<LoginState> {
@@ -15,21 +17,14 @@ export async function signin(prevState: LoginState , formData: FormData): Promis
         }
     }
 
-    //call backend api to login user
-    const { email, password } = validatedFields.data
+    const user = await authenticateUser(validatedFields.data)
 
-    const res = await fetch(`${process.env.BACKEND_URL}/api/auth/login`, {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ email, password }),
-    });
- 
-    if (!res.ok) {
+    if (!user) {
         //due security we are showing one error message
         return { errors:  "Invalid Email or Password", message: "Invalid Email or Password" }
     }
     //TODO: get token from external backend
-    // await createSession(user.id)
+    await createSession(user.access_token, user.refresh_token)
     redirect('/profile')
  
 }
