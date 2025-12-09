74f4197ce8377cbc4cf8cff07e0c2cf41ddaf8c5
enriqueFcoG <enrikefrancisco@gmail.com>
2025-12-09 13:35:57 -0800

    frontend/feat: users and auth services added

---
+++ frontend/src/services/auth.service.ts
@@ -0,0 +1,21 @@
+export const  authenticateUser = async ({ email, password }: {email:string, password:string}) => {
+  const res = await fetch(`${process.env.BACKEND_URL}/api/auth/login`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ email, password }),
+  });
+
+  if (!res.ok) return null;
+
+  const user = await res.json();
+
+  // Example of domain logic:
+  if (!user.access_token) return null;
+
+  return {
+    id: user?.id,
+    email: user?.email,
+    access_token: user?.access_token,
+    refresh_token: user?.refresh_token,
+  };
+}
---
+++ frontend/src/services/users.service.ts
@@ -0,0 +1,63 @@
+"use server"
+
+import { cookies } from "next/headers";
+
+export const  getUsers = async () => {
+  const cookieStore = cookies();
+  const token = (await cookieStore).get("access_token")?.value;
+
+  const res = await fetch(`${process.env.BACKEND_URL}/api/users`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    cache: "no-store",
+  });
+
+  if (!res.ok) return null;
+
+  const users = await res.json();
+
+  return users
+}
+
+export const getUser = async (id: number) => {
+  const cookieStore = cookies();
+  const token = (await cookieStore).get("access_token")?.value;
+
+  const res = await fetch(`${process.env.BACKEND_URL}/api/users/${id}`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    cache: "no-store",
+  });
+  
+  if (!res.ok) return null;
+
+  const users = await res.json();
+
+  return users
+}
+
+export const updateUser = async (id: number) => {
+  const cookieStore = cookies();
+  const token = (await cookieStore).get("access_token")?.value;
+
+  const res = await fetch(`${process.env.BACKEND_URL}/api/users/${id}`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    cache: "no-store",
+  });
+  
+  if (!res.ok) return null;
+
+  const users = await res.json();
+
+  return users
+}
