bbe0655a3d05afa111f1318851d0fa761d2e118d
enriqueFcoG <enrikefrancisco@gmail.com>
2025-12-09 14:41:43 -0800

    frontend/refactor: register UI changed

--- frontend/src/app/(auth)/register/page.tsx
+++ frontend/src/app/(auth)/register/page.tsx
@@ -1,47 +1,71 @@
 "use client";
 
 import { signup } from "@/actions/auth/register";
+import Link from "next/link";
 import { useActionState } from "react";
 
 const Register = () => {
   const [state, action, pending] = useActionState(signup, undefined)
   
-  return <form action={action}>
-      {pending && <p>Creating your account...</p>}
-      <div>
-        <label htmlFor="firstName">First name</label>
-        <input id="firstName" name="firstName" placeholder="First Name" />
-      </div>
-      {state?.errors?.firstName && <p>{state.errors.firstName}</p>}
+  return (
+    <div className="bg-white p-4 rounded-xl">
+      <form action={action}>
+    <p className="text-4xl font-bold text-center text-secondary-light">Create your account</p>
+    <br></br>
 
-      <div>
-        <label htmlFor="lastName">Last Name</label>
-        <input id="lastName" name="lastName" placeholder="Last Name" />
-      </div>
-      {state?.errors?.lastName && <p>{state.errors.lastName}</p>}
+    <div className="p-2">
+      <label htmlFor="firstName" className="text-secondary-light">First Name</label>
+        <div className=" flex min-w-lg rounded-l border border-stone-500/50 h-[40px] bg-white focus-within:border-cyan-500 transition-all duration-200">
+          <input id="firstName" name="firstName" className="pl-2 ml-2 w-full border-none outline-none text-stone-500 " />
+        </div>
+      {state?.errors?.firstName && <p className="text-red-500" >{state.errors.firstName}</p>}
+    </div>
 
-      <div>
-        <label htmlFor="email">Email</label>
-        <input id="email" name="email" type="email" placeholder="Email" />
-      </div>
-      {state?.errors?.email && <p>{state.errors.email}</p>}
+    <div className="p-2">
+      <label htmlFor="lastName" className="text-secondary-light">Last Name</label>
+        <div className=" flex min-w-lg rounded-l border border-stone-500/50 h-[40px] bg-white focus-within:border-cyan-500 transition-all duration-200">
+          <input id="lastName" name="lastName" className="pl-2 ml-2 w-full border-none outline-none text-stone-500 " />
+        </div>
+      {state?.errors?.lastName && <p className="text-red-500">{state.errors.lastName}</p>}
+    </div>
 
-      <div>
-        <label htmlFor="password">Password</label>
-        <input id="password" name="password" type="password" />
+    <div className="p-2">
+      <label htmlFor="email" className="text-secondary-light">Email</label>
+        <div className=" flex min-w-lg rounded-l border border-stone-500/50 h-[40px] bg-white focus-within:border-cyan-500 transition-all duration-200">
+          <input id="email" name="email" type="email" className="pl-2 ml-2 w-full border-none outline-none text-stone-500 " />
+        </div>
+      {state?.errors?.email && <p className="text-red-500">{state.errors.email}</p>}
+    </div>
+
+    <div className="p-2">
+      <label htmlFor="password" className="text-secondary-light">Password</label>
+      <div className=" flex min-w-lg rounded-l border border-stone-500/50 h-[40px] bg-white focus-within:border-cyan-500 transition-all duration-200">
+        <input id="password" name="password" type="password"className="pl-2 ml-2 w-full border-none outline-none text-stone-500 " />
       </div>
       {state?.errors?.password && (
         <div>
-          <p>Password must:</p>
+          <p className="text-red-500" >Password must:</p>
           <ul>
             {state.errors.password.map((error) => (
-              <li key={error}>- {error}</li>
+              <li key={error} className="text-red-500">- {error}</li>
             ))}
           </ul>
         </div>
       )}
-      <button type="submit">Sign Up</button>
-    </form>;
+    </div>
+
+    <div className="p-2">
+      <button type="submit" disabled={pending} className="flex-1 text-cyan-50 bg-secondary-light rounded-xl h-[40px] w-full ">Sign Up</button>
+      <p className="text-sm text-gray-600 mt-4">
+        Back to{" "}
+        <Link href="/login" className="text-blue-600">
+          Sign In
+        </Link>
+      </p>
+    </div>
+  </form>
+</div>
+  );
 };
 
 export default Register;
